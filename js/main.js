274fbda5b53c1229b5985185ebb293086ef47399
Shay Reichert <63343303+Kurtzs@users.noreply.github.com>
2020-05-17 16:44:38 +0200

    ajout des fx sur les keys et new sounds

--- js/main.js
+++ js/main.js
@@ -28,9 +28,56 @@ window.addEventListener('load', () => {
         visual.appendChild(bubble);
         bubble.style.backgroundColor = colors[index];
         bubble.style.animation = 'jump 1s ease';
-        bubble.addEventListener('animationend', function() {
+        bubble.addEventListener('animationend', function () {
             visual.removeChild(this);
         });
+    }
+
+    // play sound with Keys (wxcvbn)
+    function playSoundWithKeys(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        if (!audio) return; // don't execute if no sound
+        audio.currentTime = 0;
+        audio.play();
+    }
+    window.addEventListener('keydown', playSoundWithKeys);
+
+
+    // add "press" class to pad when keys down
+    document.onkeydown = function addClassToKeys() {
+        switch (event.keyCode) {
+            case 87:
+                $('.pad1').addClass("press");
+                break;
+            case 88:
+                $('.pad2').addClass("press");
+                break;
+            case 67:
+                $('.pad3').addClass("press");
+                break;
+            case 86:
+                $('.pad4').addClass("press");
+                break;
+            case 66:
+                $('.pad5').addClass("press");
+                break;
+            case 78:
+                $('.pad6').addClass("press");
+                break;
+        }
+    }
+    //remove "press" class to pad when keys up
+    document.onkeyup = function removeClassToKeys() {
+        switch (event.keyCode) {
+            case 87:
+            case 88:
+            case 67:
+            case 86:
+            case 66:
+            case 78:
+                $('.pad').removeClass("press");
+                break;
+        }
     }
 });
 
